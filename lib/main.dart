ec5b3f1c9cb3b520148b7e372d522a993bd868a0
TrEv0rRrRr <valentinosolischang@email.com>
2026-06-21 21:43:51 -0500

    Simplify main function and remove unnecessary NeurozenApp class definition

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:neurozen_front/features/shell/main_shell_screen.dart';
+import 'app/app.dart';
 
-void main() {
-  runApp(const NeurozenApp());
-}
-
-class NeurozenApp extends StatelessWidget {
-  const NeurozenApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Neurozen Mock',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: const Color(0xFF6FB98F),
-          brightness: Brightness.light,
-        ),
-        scaffoldBackgroundColor: const Color(0xFFF4FBF6),
-      ),
-      home: const MainShellScreen(),
-    );
-  }
-}
+void main() => runApp(const NeurozenApp());
