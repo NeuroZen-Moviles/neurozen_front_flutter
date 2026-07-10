b543175c9eba6d84f323bf2eb1307e22aa10e54e
TrEv0rRrRr <valentinosolischang@email.com>
2026-07-10 17:47:50 -0500

    Changed the color of the "log out" button to make it more noticeable

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,26 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart';
 
-void main() => runApp(const NeurozenApp());
+import 'dart:io';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  try {
+    final google = await InternetAddress.lookup("google.com");
+    debugPrint("Google: $google");
+  } catch (e) {
+    debugPrint("Google ERROR: $e");
+  }
+
+  try {
+    final render = await InternetAddress.lookup(
+      "neurozen-backend-mobile.onrender.com",
+    );
+    debugPrint("Render: $render");
+  } catch (e) {
+    debugPrint("Render ERROR: $e");
+  }
+
+  runApp(const NeurozenApp());
+}
