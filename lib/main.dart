e11c6ec7f7207e34a9fc1f56941cde08466bfb62
TrEv0rRrRr <valentinosolischang@email.com>
2026-07-10 17:55:10 -0500

    Deleting the debug messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,6 @@
 import 'package:flutter/material.dart';
 import 'app/app.dart';
 
-import 'dart:io';
-
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-
-  try {
-    final google = await InternetAddress.lookup("google.com");
-    debugPrint("Google: $google");
-  } catch (e) {
-    debugPrint("Google ERROR: $e");
-  }
-
-  try {
-    final render = await InternetAddress.lookup(
-      "neurozen-backend-mobile.onrender.com",
-    );
-    debugPrint("Render: $render");
-  } catch (e) {
-    debugPrint("Render ERROR: $e");
-  }
-
   runApp(const NeurozenApp());
 }
